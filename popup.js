24d1779af8ccf5aba0179c9142cd64f9a9bd4c3b
Dave Gumba <dave_gumba@Yahoo.com>
2017-02-04 23:41:53 -0500

    Jquery is bae

--- popup.js
+++ popup.js
@@ -1,3 +1,13 @@
+/*
+TODO: GET request a JSON file
+TODO: Draw an icon
+TODO: More positivity. Fucking positivity overflow. Fuck all negativity.
+TODO: Hundreds of positive messages
+TODO: Shit ton of colors.
+TODO: Switch up typeface
+TODO: Add to Chrome store
+*/
+
 var $messageText = $('#message');
 var $containerFluid = $('.container-fluid');
 var randBg = colors[Math.floor(Math.random() * (colors.length - 1))];
