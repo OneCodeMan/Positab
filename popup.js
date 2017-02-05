3a7bda154b2fa959b1cf7e2b6cc61176b2715ecd
Dave Gumba <dave_gumba@Yahoo.com>
2017-02-04 23:28:15 -0500

    I'm listening to Nirvana - Smells Like Teen Spirit and I am so ****ing hyper.

--- popup.js
+++ popup.js
@@ -1,15 +1,7 @@
-var messageDiv = new Vue({
-  el: '#message',
-  data: {
-    message: 'You are awesome!'
-  }
-})
+var $messageText = $('#message');
+var $containerFluid = $('.container-fluid');
+var randBg = colors[Math.floor(Math.random() * (colors.length - 1))];
 
-var containerDiv = new Vue({
-  el: '.container-fluid',
-  data: {
-    bgColor: {
-      backgroundColor: colors[Math.floor(Math.random() * (colors.length - 1))],
-    }
-  },
-});
+$containerFluid.css('background-color', randBg);
+
+$messageText.text('You\'re awesome as fuck.');
